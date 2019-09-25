d806adf216f8c7fd34d0f68dde9e3729e09cc8fc
3uxeca <sajihye4@gmail.com>
2019-09-25 17:28:33 +0900

    09/25

--- Django_code/3-1_urlconf_ex.py
+++ Django_code/3-1_urlconf_ex.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('')
+    path('articles/2003/', views.special_case_2003),
+    path('articles/<int:year>/', views.year_archive),
+    path('articles/<int:year>/<int:month>/', views.month_archive),
+    path('articles/<int:year>/<int:month>/<slug:slug>/', views.article_detail),
 ]
--- Django_code/example.py
+++ Django_code/example.py
@@ -1,10 +1,28 @@
 # import urllib.request
 # print(urllib.request.urlopen("http://www.example.com").read().decode('utf-8'))
 
-from django.db import models
+# from django.db import models
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+# class Person(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
 
-    
+
+
+# import pytz
+
+# for tz in pytz.all_timezones:
+#     print(tz)
+
+
+import datetime
+from pytz import timezone, utc
+
+KST = timezone('Asia/Seoul')
+now = datetime.datetime.utcnow()
+
+print(utc.localize(now))
+
+print(KST.localize(now))
+
+print(utc.localize(now).astimezone(KST))
